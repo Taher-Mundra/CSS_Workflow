da198de13596d83b77bd79b09ebabd042f654008
Taher Mundrawala <taherraja41@gmail.com>
2024-04-28 13:22:13 -0400

    All the styles and html completed

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    //hamburger menu toggle
+    $('.nav-toggle').click(()=>{
+        $('.main-nav').toggleClass('is-open')
+        $('.hamburger').toggleClass('is-open')
+    })
+})
